c4bbad0a739d3e2ba5ba938cf14264c00bc3b1fb
Jeffrey Kim <jeffreyk93@gmail.com>
2021-03-10 17:14:41 -0500

    Edited site name

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://website.com';
+export const server = dev ? 'http://localhost:3000' : 'https://twitter-clone-site.vercel.app';
