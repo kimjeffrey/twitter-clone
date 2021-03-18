8b57d741b2857c189a661963bf65c2dee4639402
Jeffrey Kim <jeffreyk93@gmail.com>
2021-03-17 21:36:59 -0400

    Added navbar to view tweets, replies, likes on profile page

--- pages/profile/[id]/index.js
+++ pages/profile/[id]/index.js
@@ -1,12 +1,29 @@
 import {server} from '../../../config'
+import {useEffect, useState} from 'react'
 import {useSession} from 'next-auth/client'
 import Meta from '../../../components/Meta'
 import Post from '../../../components/Post'
 import Tweet from '../../../components/Tweet'
+import styles from '../../../styles/ProfilePage.module.scss'
 
 export default function profile({user}) {
   const [session] = useSession();
 
+  const [navSelection, setNavSelection] = useState("Tweets");
+
+  function handleClick(event) {
+    switch(event.target.innerText) {
+      case "Replies":
+        setNavSelection("Replies");
+        break;
+      case "Likes":
+        setNavSelection("Likes");
+        break;
+      default:
+        setNavSelection("Tweets");
+    }
+  }
+  
   function sortByNew(){
     let sortedPosts = [...user.posts];
     sortedPosts.sort((a, b) => {
@@ -21,7 +38,12 @@ export default function profile({user}) {
       {session.id === user._id &&
         <Tweet />
       }
-      {user.posts && sortByNew().map((post, index) => (
+      <nav className={styles.navbar}>
+        <div onClick={handleClick} className={navSelection === "Tweets" ? `${styles.navItem} ${styles.selected}` : styles.navItem}>Tweets</div>
+        <div onClick={handleClick} className={navSelection === "Replies" ? `${styles.navItem} ${styles.selected}` : styles.navItem}vvvv>Replies</div>
+        <div onClick={handleClick} className={navSelection === "Likes" ? `${styles.navItem} ${styles.selected}` : styles.navItem}>Likes</div>
+      </nav>
+      {navSelection === "Tweets" && user.posts && sortByNew().map((post, index) => (
         <Post key={index} id={post._id} user={user._id} name={user.name} content={post.content} date={post.date.toString()} likes={post.likes} replies={post.replies.length} />
       ))}
     </>
